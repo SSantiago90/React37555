ac628a8c46c2b7726df3482ff0322a4dc56736e0
ssantiago90 <salkin.santiago@gmail.com>
2022-09-06 13:46:25 -0300

    clase 3 JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+  let title = "Hola Comision 37555";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/assets/logos_coderhouse.png" alt="logo" />
+        <h1>{title}</h1>
       </header>
+      <Footer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <>
+    <footer className="footer">
+      <h2>
+        <strong>Mi Ecommerce</strong>
+      </h2>
+      <ul>
+        <li>Inicio</li>
+        <li>Contacto</li>
+        <li>Servicios</li>
+      </ul>
+    </footer>
+    <span>Copytight ....</span>
+    </>
+  );
+}
+
+export default Footer;
